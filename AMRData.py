684acad71066de3c5391485cb974ef4af39c0904
Andreea <andreea.lazar04@yahoo.com>
2017-01-03 21:37:04 +0200

    customized amr

--- AMRData.py
+++ AMRData.py
@@ -15,21 +15,23 @@ class CustomizedAMR():
     def __init__(self):
         self.dict ={}
 
-    def add_parent(self, key, parent):
+    def add_parent_and_edge(self, key, parent, edge):
         if key not in self.dict.keys():
             data = AMRData()
             data.parent = parent
+            data.edgeToParent= edge
             self.dict[key] = data
         else:
             self.dict[key].parent = parent
+            self.dict[key].edgeToParent = edge
 
     def createCustomAMR(self, amrgraph):
         for item in amrgraph.items():
             data = AMRData()
-            concept = amrgraph.node_to_concepts[item[0]]
-            for value in item[1].itervalues():
+            concept = item[0]
+            for key, value in item[1].iteritems():
                 data.add_child(value[0][0])
-                self.add_parent(value[0][0], concept)
+                self.add_parent_and_edge(value[0][0], concept, key)
 
 
 
