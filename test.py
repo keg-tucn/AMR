61b6b823726721d7492e3a147373e361f501ad10
Andreea <andreea.lazar04@yahoo.com>
2017-01-07 20:23:01 +0200

    solved conflict

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 from AMRGraph import AMR
-from AMRData import CustomizedAMR
 
 amr = AMR.parse_string('(l / look-01~e.3,5 '
       ':ARG0 (i / i~e.0) '
       ':mod (j / just~e.2))')
-camr=CustomizedAMR()
-camr.createCustomAMR(amr)
 
