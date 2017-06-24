a2fcb122e5ebea31228b7553cd63ce9256e7cf4f
flo <flo.macicasan@gmail.com>
2017-06-24 20:43:51 +0300

    extract node in dedicated to reuse in parsers

--- ActionSequenceReconstruction.py
+++ ActionSequenceReconstruction.py
@@ -3,6 +3,7 @@ from AMRData import CustomizedAMR
 from deep_dynet import support
 import ActionSequenceGenerator
 from collections import deque
+from Node import Node
 
 VOCAB_ACTS = ['SH', 'RL', 'RR', 'DN', 'SW']
 SH = 0
@@ -92,31 +93,6 @@ class ReconstructionState:
     def finalize(self):
         top = self.stack.pop()
         return top
-
-
-class Node:
-    def __init__(self, label, node_index):
-        self.label = label
-        self.children = []
-        self.node_index = node_index
-
-    def add_child(self, obj, relation):
-        self.children.append((obj, relation))
-
-    def amr_print(self, depth=1):
-        str = "( d%s / %s " % (self.node_index, self.label)
-        for (child, relation) in self.children:
-            if relation == "polarity" or relation == "mode":
-                child_representation = ":%s %s" % (relation, child.label)
-            else:
-                child_representation = ":%s  %s" % (relation, child.amr_print(depth + 1))
-            str += "\n".ljust(depth + 1, "\t") + child_representation
-
-        if self.children:
-            str += "\n".ljust(depth, "\t")
-        str += ")"
-        return str
-
 
 if __name__ == "__main__":
 
