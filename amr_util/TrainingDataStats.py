319ba3359499e431004e96eb5ce3ad624ce5aa41
flo <flo.macicasan@gmail.com>
2017-11-27 01:59:23 +0200

    adds training data stats to model results from sequence generation

--- amr_util/TrainingDataStats.py
+++ amr_util/TrainingDataStats.py
@@ -13,3 +13,17 @@ def get_unaligned_nodes(amr, unaligned_nodes_dict):
 def get_coreferences_count(custom_amr):
     keys = [k[0] for k in custom_amr.relations_dict.keys()]
     return len(keys) - len(set(keys))
+
+
+class TrainingDataStatistics:
+
+    def __init__(self, unaligned_nodes, unaligned_nodes_after, coreferences_count, named_entity_exceptions,
+                 date_entity_exceptions, temporal_quantity_exceptions, quantity_exceptions, have_org_role_exceptions):
+        self.unaligned_nodes = unaligned_nodes
+        self.unaligned_nodes_after = unaligned_nodes_after
+        self.coreferences_count = coreferences_count
+        self.named_entity_exceptions = named_entity_exceptions
+        self.date_entity_exceptions = date_entity_exceptions
+        self.temporal_quantity_exceptions = temporal_quantity_exceptions
+        self.quantity_exceptions = quantity_exceptions
+        self.have_org_role_exceptions = have_org_role_exceptions
