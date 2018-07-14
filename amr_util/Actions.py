54b8736fd5b0807941bcb34b0a6a96f5f4451ed0
Ranapop <roxy_rox1995@yahoo.com>
2018-07-14 11:36:26 +0300

    added demo for action sequence generation

--- amr_util/Actions.py
+++ amr_util/Actions.py
@@ -14,6 +14,13 @@ class AMRAction:
     def __repr__(self):
         return "action: %s label: %s label2: %s index: %s key: %s key2: %s" % (self.action, self.label, self.label2, self.index, self.key, self.key2)
 
+    def to_string(self):
+        if not self.label:
+            return self.action
+        if not self.label2:
+            return self.action+"_"+self.label
+        return self.action+"_"+self.label+"_"+self.label2
+
     @classmethod
     def build(cls, action):
         return AMRAction(action, None, None)
@@ -21,6 +28,7 @@ class AMRAction:
     @classmethod
     def build_labeled(cls, action, label):
         return AMRAction(action, label, None)
+
 
 if __name__ == "__main__":
 
---
+++ preprocessing/action_sequence_generators/pipelining_asg.py
@@ -0,0 +1,89 @@
+# test what instances are processed by different asg versions
+from preprocessing import SentenceAMRPairsExtractor
+
+from amr_util.demo_util import get_custom_amr
+from amr_util.demo_util import get_smatch
+from postprocessing import ActionSequenceReconstruction as asr
+from AMRGraph import AMR
+import AMRData
+from preprocessing.action_sequence_generators.simple_asg import SimpleASG
+from preprocessing.action_sequence_generators.simple_asg__informed_swap import SimpleInformedSwapASG
+from preprocessing.action_sequence_generators.simple_informed_break_nodes_on_stack import SimpleInformedWithBreakNodesOnStackASG
+from preprocessing.action_sequence_generators.backtracking_asg import BacktrackingASGFixedReduce
+
+bolt_file_path = '/home/andrei/dynet-base/AMR_lic/resources/alignments/split/training/deft-p2-amr-r2-alignments-training-bolt.txt'
+
+datasets = ["bolt","cctv","dfa","dfb","guidelines","mt09sdl","proxy","wb","xinhua"]
+
+for i in range(len(datasets)):
+
+    file_path = '/home/andrei/dynet-base/AMR_lic/resources/alignments/split/training/deft-p2-amr-r2-alignments-training-'+datasets[i]+'.txt'
+
+    sentence_amr_triples = SentenceAMRPairsExtractor.extract_sentence_amr_pairs(file_path)
+    for i in range(0, len(sentence_amr_triples)):
+        (sentence, amr_str, amr_id) = sentence_amr_triples[i]
+        sentence_len = len(sentence.split(" "))
+        if sentence_len < 20:
+            try:
+                amr = AMR.parse_string(amr_str)
+                custom_amr = AMRData.CustomizedAMR()
+                custom_amr.create_custom_AMR(amr)
+
+                try:
+                    #initial asg
+                    asg_implementation = SimpleASG(1, False)
+                    action_sequence = asg_implementation.generate_action_sequence(custom_amr, sentence)
+                    #print(amr_id)
+                except:
+                    try:
+                        # informed swap (1 swap)
+                        asg_implementation = SimpleInformedSwapASG(1,should_rotate=False)
+                        action_sequence = asg_implementation.generate_action_sequence(custom_amr, sentence)
+                        #print(action_sequence)
+                        #print(amr_id)
+                    except:
+                        try:
+                            # informed swap (2 swap)
+                            asg_implementation = SimpleInformedSwapASG(2, should_rotate=False)
+                            action_sequence = asg_implementation.generate_action_sequence(custom_amr, sentence)
+                            #print(action_sequence)
+                            #print(amr_id)
+                        except:
+                            try:
+                                # informed swap (3 swaps)
+                                asg_implementation = SimpleInformedSwapASG(3, should_rotate=False)
+                                action_sequence = asg_implementation.generate_action_sequence(custom_amr, sentence)
+                                #print(action_sequence)
+                                #print(amr_id)
+                            except:
+                                try:
+                                    # informed swap (rotate)
+                                    asg_implementation = SimpleInformedSwapASG(3, should_rotate=True)
+                                    action_sequence = asg_implementation.generate_action_sequence(custom_amr, sentence)
+                                    #print(action_sequence)
+                                    #print(amr_id)
+                                except:
+                                    try:
+                                        # informed swap with break-token
+                                        asg_implementation = SimpleInformedWithBreakNodesOnStackASG(1, should_rotate=False)
+                                        action_sequence = asg_implementation.generate_action_sequence(custom_amr, sentence)
+                                        #print(amr_id)
+                                        #print(action_sequence)
+                                    except:
+                                        pass
+                        # backtracking
+                        if sentence_len < 10:
+                            try:
+                                max_depth = 4 * sentence_len
+                                asg_implementation = BacktrackingASGFixedReduce(1, max_depth)
+                                action_sequence = asg_implementation.generate_action_sequence(custom_amr, sentence)
+                                generated_amr_str = asr.reconstruct_all(action_sequence)
+                                smatch_f_score = get_smatch(generated_amr_str, amr_str)
+                                if smatch_f_score == 1:
+                                    print(amr_id)
+                                    print(amr_str)
+                                    print(action_sequence)
+                            except:
+                                pass
+            except:
+                pass
