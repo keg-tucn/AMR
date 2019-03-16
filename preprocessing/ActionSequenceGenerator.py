3305c75fa0357667b2410799256663098015c15e
ieremiasviorel <ieremiasviorel@yahoo.com>
2019-03-17 00:29:31 +0200

    Fix performance bug with DependencyExtractor
    - make Parser instance in DependencyExtractor module a class variable, so that it is not instantiated for each sentence

--- preprocessing/ActionSequenceGenerator.py
+++ preprocessing/ActionSequenceGenerator.py
@@ -1,4 +1,5 @@
 import logging
+
 import models.Actions as act
 
 
--- preprocessing/DependencyExtractor.py
+++ preprocessing/DependencyExtractor.py
@@ -1,30 +1,30 @@
-from os import path
-import sys
 from itertools import groupby
 
-sys.path.append(path.abspath('./stanford_parser'))
 from stanford_parser.parser import Parser
 
 
-def splitWithIndices(string, splitChar=' '):
-    p = 0
-    start_indices = {}
-    current_token = 0
-    for k, g in groupby(string, lambda x: x == splitChar):
-        q = p + sum(1 for i in g)
-        if not k:
-            start_indices[p] = current_token
-            current_token += 1
-        p = q
-    return start_indices
+class DependencyExtractor:
+    stanford_parser = Parser()
 
+    @classmethod
+    def extract_dependencies(cls, sentence):
+        indices = DependencyExtractor._split_with_indices(sentence)
+        dependencies = DependencyExtractor.stanford_parser.parseToStanfordDependencies(sentence.strip())
+        dependencies_dict = {}
+        for (rel, gov, dep) in dependencies.dependencies:
+            if dep.start in indices.keys() and gov.start in indices.keys():
+                dependencies_dict[indices[dep.start]] = (indices[gov.start], rel)
+        return dependencies_dict
 
-def extract_dependencies(sentence):
-    parser = Parser()
-    indices = splitWithIndices(sentence)
-    dependencies = parser.parseToStanfordDependencies(sentence.strip())
-    dependencies_dict = {}
-    for (rel, gov, dep) in dependencies.dependencies:
-        if dep.start in indices.keys() and gov.start in indices.keys():
-            dependencies_dict[indices[dep.start]] = (indices[gov.start], rel)
-    return dependencies_dict
+    @classmethod
+    def _split_with_indices(cls, string, split_char=' '):
+        p = 0
+        start_indices = {}
+        current_token = 0
+        for k, g in groupby(string, lambda x: x == split_char):
+            q = p + sum(1 for i in g)
+            if not k:
+                start_indices[p] = current_token
+                current_token += 1
+            p = q
+        return start_indices
