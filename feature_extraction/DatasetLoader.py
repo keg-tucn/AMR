16ed91a090e334795a8579643b84bc4e28c57646
ieremiasviorel <ieremiasviorel@yahoo.com>
2019-04-15 11:37:37 +0300

    - add method in feature_extraction.DatasetLoader to load and pickle pairs of parsed AMRGraph and CustomizedAMR objects - add labels_learner.DatasetExtractor to extract (parent_node, child_node) -> relation dictionary - save concepts_relations dictionary to resources-> concepts_relations -> concepts_relations_dict

--- feature_extraction/DatasetLoader.py
+++ feature_extraction/DatasetLoader.py
@@ -2,7 +2,59 @@ from os import listdir, path, makedirs
 import pickle as js
 
 from definitions import AMR_ALIGNMENTS_SPLIT
+from models.AMRGraph import AMR
+from models.AMRData import CustomizedAMR
+from preprocessing import SentenceAMRPairsExtractor
 from TrainingDataExtractor import generate_training_data
+
+
+def read_original_graphs(type, filter_path="deft", cache=True):
+    if filter_path is None:
+        filter_path = "deft"
+    dir_path = AMR_ALIGNMENTS_SPLIT + "/" + type
+    print(dir_path + " with filter " + filter_path)
+
+    parsed_data = []
+
+    directory_content = listdir(dir_path)
+    original_corpus = filter(lambda x: "dump" not in x and filter_path in x, directory_content)
+
+    for file_name in original_corpus:
+        original_file_path = dir_path + "/" + file_name
+        dump_file_path = dir_path + "/original_graphs_dumps/" + file_name + ".dump"
+        print(original_file_path)
+
+        if cache and path.exists(dump_file_path):
+            print("cache")
+            with open(dump_file_path, "rb") as dump_file:
+                parsed_data += js.load(dump_file)
+        else:
+            file_data = SentenceAMRPairsExtractor.extract_sentence_amr_pairs(original_file_path)
+
+            parsed_file_data = []
+            failed_amrs_in_file = 0
+
+            for amr_triple in file_data:
+                try:
+                    camr_graph = AMR.parse_string(amr_triple[1])
+
+                    custom_amr_graph = CustomizedAMR()
+                    custom_amr_graph.create_custom_AMR(camr_graph)
+
+                    parsed_file_data.append((amr_triple[2], amr_triple[0], camr_graph, custom_amr_graph))
+                except Exception as _:
+                    # print "Exception when parsing AMR with ID: %s in file: %s\n" % (amr_triple[2], original_file_path)
+                    failed_amrs_in_file += 1
+
+            if not path.exists(path.dirname(dump_file_path)):
+                makedirs(path.dirname(dump_file_path))
+            with open(dump_file_path, "wb") as dump_file:
+                js.dump(parsed_file_data, dump_file)
+            parsed_data += parsed_file_data
+
+            print "%d / %d in %s" % (failed_amrs_in_file, len(file_data), original_file_path)
+
+    return parsed_data
 
 
 def read_data(type, filter_path="deft", cache=True):
---
+++ labels_learner/DatasetExtractor.py
@@ -0,0 +1,56 @@
+import pickle as js
+
+from definitions import CONCEPTS_RELATIONS_DICT
+from feature_extraction import DatasetLoader
+
+
+def extract_concepts_relations_pairs():
+    """
+    Return a dictionary mapping (parent_concept, child_concept) -> parent_child_AMR_relation
+    after parsing the entire dataset and also save the dictionary in the resources folder
+    """
+    dataset_identifier = "r2"
+    dataset_splits = ["training", "dev", "test"]
+
+    concepts_relations_dict = dict()
+
+    for data_split in dataset_splits:
+        graphs_data = DatasetLoader.read_original_graphs(type=data_split, filter_path=dataset_identifier)
+
+        for graph_data in graphs_data:
+            amr_graph = graph_data[2]
+
+            for concept_relations in amr_graph.items():
+                for concept_relation in concept_relations[1].items():
+                    parent_token = concept_relations[0]
+                    child_token = concept_relation[1][0]
+
+                    parent_concept = amr_graph.node_to_concepts[parent_token]
+                    if child_token in amr_graph.node_to_concepts:
+                        child_concept = amr_graph.node_to_concepts[child_token]
+                    else:
+                        child_concept = child_token
+
+                    if (parent_concept, child_concept) in concept_relations:
+                        concepts_relations_dict[(parent_concept, child_concept)].append(concept_relation[0])
+                    else:
+                        concepts_relations_dict[(parent_concept, child_concept)] = [concept_relation[0]]
+
+    with open(CONCEPTS_RELATIONS_DICT, "wb") as dict_file:
+        js.dump(concepts_relations_dict, dict_file)
+
+    return concepts_relations_dict
+
+
+if __name__ == "__main__":
+
+    concepts_rels_dict = extract_concepts_relations_pairs()
+
+    multiple_rels_pairs = 0
+
+    for item in concepts_rels_dict.items():
+        if len(item[1]) > 1:
+            print str(item[0]) + " | " + str(item[1])
+            multiple_rels_pairs += 1
+
+    print "%d / %d" % (multiple_rels_pairs, len(concepts_rels_dict.items()))
