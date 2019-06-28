3cc7db8129514d45706d32ed67828eb337181fb2
a_sabau <sabau.andrei14@yahoo.com>
2019-06-28 19:35:14 +0300

    changed extraction function to work with coref datasets

--- preprocessing/SentenceAMRPairsExtractor.py
+++ preprocessing/SentenceAMRPairsExtractor.py
@@ -1,13 +1,25 @@
 import re
 
 
-def extract_sentence_amr_pairs(file_path):
+def extract_sentence_amr_pairs(file_path, extract_from_coref_datasets=False):
     with open(file_path) as f:
         lines = f.readlines()
-    token_regex = re.compile('^(?:# ::tok )(.*)$')
-    amr_start_indices = [index for index in range(0, len(lines)) if token_regex.match(lines[index])]
 
-    triples = map(lambda i: (token_regex.match(lines[i]).group(1), get_amr(lines, i), get_id(lines, i)), amr_start_indices)
+    if extract_from_coref_datasets:
+        token_regex = re.compile('^(?:# sentence: )(.*)$') # this is how I find data in my dataset which has only senteces and AMRs with corefs
+
+        amr_start_indices = [index for index in range(0, len(lines)) if token_regex.match(lines[index])]
+
+        triples = map(lambda i: (token_regex.match(lines[i]).group(1), get_amr(lines, i), 1),
+                      amr_start_indices)
+
+    else:
+        token_regex = re.compile('^(?:# ::tok )(.*)$')
+
+        amr_start_indices = [index for index in range(0, len(lines)) if token_regex.match(lines[index])]
+
+        triples = map(lambda i: (token_regex.match(lines[i]).group(1), get_amr(lines, i), get_id(lines, i)), amr_start_indices)
+
     return triples
 
 
