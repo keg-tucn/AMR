448981dd02b6845f513d91b39054f7a866ac995c
Ranapop <roxy_rox1995@yahoo.com>
2020-04-30 15:09:07 +0300

    extracted method for reading glove embeddings from file

--- data_extraction/word_embeddings_reader.py
+++ data_extraction/word_embeddings_reader.py
@@ -45,9 +45,11 @@ def get_token_embedding_from_full(token):
     return full_embeddings_matrix_dict.get(token)
 
 
-def _load_embeddings_matrix(word_index, embedding_dim):
-    special_cases_re = re.compile("""^([a-z])+-(?:entity|quantity)$""")
-
+def read_glove_embeddings_from_file(embedding_dim):
+    """
+    Read glove embedding from file corresponding to embedding_dim (can be 50, 100, 200, 300)
+    Return dictionary of word -> glove_embedding
+    """
     embeddings_index = {}
 
     emb_file = open(GLOVE_EMBEDDINGS + "/" + "glove.6B.{}d.txt".format(embedding_dim), encoding="utf-8")
@@ -57,6 +59,13 @@ def _load_embeddings_matrix(word_index, embedding_dim):
         coefs = np.asarray(values[1:], dtype="float32")
         embeddings_index[word] = coefs
     emb_file.close()
+    return embeddings_index
+
+
+def _load_embeddings_matrix(word_index, embedding_dim):
+    special_cases_re = re.compile("""^([a-z])+-(?:entity|quantity)$""")
+
+    embeddings_index = read_glove_embeddings_from_file(embedding_dim)
 
     print(("Found %s word vectors." % len(embeddings_index)))
 
