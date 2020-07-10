7ca3d3f534159006c42f13b936ed47e81d7d1b48
Ranapop <roxy_rox1995@yahoo.com>
2020-07-10 18:08:51 +0300

    variable renaming

--- trainers/nodes/concept_extraction/sequence_to_sequence_ordered_concept_extraction/pointer_generator_concept_extractor/pointer_generator_trainer.py
+++ trainers/nodes/concept_extraction/sequence_to_sequence_ordered_concept_extraction/pointer_generator_concept_extractor/pointer_generator_trainer.py
@@ -559,8 +559,8 @@ if __name__ == "__main__":
 
     EXP_RUN = False
     TRAIN = True
-    TRAIN_ON_20 = False
-    if TRAIN_ON_20:
+    RUN_ON_20 = False
+    if RUN_ON_20:
         max_sen_len = 20
         no_epochs = 30
     else:
